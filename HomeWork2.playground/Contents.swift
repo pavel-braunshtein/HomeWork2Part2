b1b92b83618e647f4cffa2245a51aa0b3a5cf26c
pavelbraun333 <pavelbraun333@gmail.com>
2024-02-26 00:18:16 +0200

    2nd screen has been completed

--- HomeWork2.playground/Contents.swift
+++ HomeWork2.playground/Contents.swift
@@ -242,15 +242,15 @@ print("\n\n")
 // виводимо інформацію про температуру тільки у Цельсій
 // для кожного міста, що є у масиві weatherInCities
 print("---------- ЕКРАН 2 ----------")
-for index in 1 ..< 10 {
-    let _ = index
-    let weatherInfo = weatherInCities[2]
+for weatherInfo in weatherInCities {
     let city = weatherInfo.0
     let kelvin = weatherInfo.1
-    print("\n\(city):\nt: \(String(format: "%.1f", kelvin)) C")
+    let celcius = kelvin - 273.15
+    print("\n\(city):\nt: \(String(format: "%.1f", celcius)) C")
 }
 print("\n-----------------------------")
 print("\n\n")
+
 
 print("---------- ЕКРАН 3 ----------")
 for index in 0 ... weatherInCities.count {
